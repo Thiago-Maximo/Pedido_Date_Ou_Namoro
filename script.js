679262fc68f5d926aa7779f396b5021aed7410ab
Thiago-Maximo <142333508+Thiago-Maximo@users.noreply.github.com>
2025-07-24 14:56:56 -0300

    Removido uma linha
    Removido uma linha que aparentemente era desnecessária

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ document.addEventListener('DOMContentLoaded', function() {
     
     // Botões que redirecionam para WhatsApp
     function sendWhatsAppMessage(isAccepted) {
-        const phoneNumber = 'SEU_NUMERO_AQUI'; // Substitua pelo seu número com código do país
         const acceptMsg = 'Estou aceitando seu convite para o encontro!'; // mensagem que vc ira receber se a pessoa aceitar o convite/pedido
         const declineMsg = 'Obrigada pelo convite, mas preciso recusar.'; // mensagem que vc ira receber se a pessoa recusar o convite/pedido
         const message = isAccepted ? acceptMsg : declineMsg;
